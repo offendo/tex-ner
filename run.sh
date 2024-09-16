1072b967d83066837b55b0a894637e22744d2c22
Nilay Patel <nilaypatel2@gmail.com>
2024-09-16 12:30:12 -0700

    Update epoch count for run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,9 @@ wandb login $(cat /etc/api-tokens/wandb-token)
 python src/ner_training/main.py train \
     --model FacebookAI/roberta-base \
     --definition --theorem --proof --example --name \
+    --epochs 25 \
     --data_dir /volume/ner/ \
-    --output_dir /volume/ner/outputs/
+    --output_dir /volume/ner/outputs/ \
 
 # Run testing
 python src/ner_training/main.py test \
