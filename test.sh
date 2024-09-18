8333286102ddce1308599a26dc6009506e8b9ca7
Nilay Patel <nilaypatel2@gmail.com>
2024-09-18 12:43:50 -0700

    add test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Login
+huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
+wandb login $(cat /etc/api-tokens/wandb-token)
+
+echo "Beginning run $RUN_NAME"
+mkdir -p "/volume/ner/outputs/$RUN_NAME"
+
+# Run testing
+export WANDB_RUN_NAME="$RUN_NAME"
+python src/ner_training/main.py test \
+    --model FacebookAI/roberta-base \
+    --checkpoint /volume/ner/outputs/$RUN_NAME/checkpoint-final \
+    --definition --theorem --proof --example \
+    --data_dir /volume/ner \
+    --output_dir /volume/ner/outputs/$RUN_NAME
+
