99c63867755782e9506df4d4293865f6029547c6
Nilay Patel <nilaypatel2@gmail.com>
2024-09-24 19:13:14 -0700

    move run name to train-job.yaml

--- run.sh
+++ run.sh
@@ -5,7 +5,11 @@ huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
 wandb login $(cat /etc/api-tokens/wandb-token)
 
 # Determine run name
-RUN_NAME=$(curl https://random-word-api.herokuapp.com/word?number=2 | tr '[,"]' '-' | sed 's/--//g')
+#RUN_NAME=$(curl https://random-word-api.herokuapp.com/word?number=2 | tr '[,"]' '-' | sed 's/--//g')
+if [[ -z $RUN_NAME ]]; then
+  echo "Error: empty RUN_NAME"
+  exit 1
+fi
 
 echo "Beginning run $RUN_NAME"
 mkdir -p "/volume/ner/outputs/$RUN_NAME"
