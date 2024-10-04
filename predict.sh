1d35923783f192f3a54fa876d0b5419e5ad1e60d
Nilay Patel <nilaypatel2@gmail.com>
2024-10-04 10:01:36 -0700

    Install file beforehand

--- predict.sh
+++ predict.sh
@@ -4,6 +4,9 @@
 huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
 wandb login $(cat /etc/api-tokens/wandb-token)
 
+# Update the code
+pip install -U .
+
 echo "Beginning run $RUN_NAME"
 mkdir -p "/volume/ner/outputs/$RUN_NAME"
 mkdir -p "/volume/ner/outputs/mmd-preds/"
