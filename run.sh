a38f48f3dfb4676c5f6efa7b76068db1bbfb767e
Nilay Patel <nilaypatel2@gmail.com>
2024-10-26 14:32:29 -0700

    Add parallelism to jobs

--- run.sh
+++ run.sh
@@ -17,59 +17,54 @@ if [[ -z $NTRIALS ]]; then
   NTRIALS=1
 fi
 
-for ((i=1; i<=$NTRIALS; i++)); do
+if [[ $JOB_COMPLETION_INDEX != "" ]]; then
+  ITER_NAME=$RUN_NAME-$JOB_COMPLETION_INDEX
+else
+  ITER_NAME=$RUN_NAME
+fi
+echo "Beginning run $ITER_NAME"
+mkdir -p "/volume/ner/outputs/$ITER_NAME"
 
-  if [[ $NTRIALS = 1 ]]; then
-    ITER_NAME=$RUN_NAME
-  else
-    ITER_NAME=$RUN_NAME-$i
+# Run training
+export WANDB_NAME="$RUN_NAME"
+if [[ $DO_TRAIN = 'true' ]]; then
+    python src/ner_training/main.py --run_train \
+      --model_name_or_path FacebookAI/roberta-base \
+      $CRF \
+      $CLASSES \
+      $TRAIN_FLAGS \
+      --learning_rate $LEARNING_RATE \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --eval_steps 250 --eval_strategy "steps" \
+      --logging_strategy "steps" --logging_steps 10 \
+      --label_smoothing_factor $LABEL_SMOOTHING --warmup_ratio $WARMUP_RATIO --weight_decay $WEIGHT_DECAY --dropout $DROPOUT \
+      --load_best_model_at_end True --metric_for_best_model "eval_f1" \
+      --optim "adamw_hf" --lr_scheduler_type $SCHEDULER \
+      --data_dir /volume/ner/$DATASET --output_dir /volume/ner/outputs/$ITER_NAME
   fi
 
-  echo "Beginning run $ITER_NAME"
-  mkdir -p "/volume/ner/outputs/$ITER_NAME"
-
-  # Run training
-  export WANDB_RUN_NAME="$RUN_NAME"
-  if [[ $DO_TRAIN = 'true' ]]; then
-     python src/ner_training/main.py --run_train \
-       --model_name_or_path FacebookAI/roberta-base \
-       $CRF \
-       $CLASSES \
-       $TRAIN_FLAGS \
-       --learning_rate $LEARNING_RATE \
-       --per_device_train_batch_size $BATCH_SIZE \
-       --per_device_eval_batch_size $BATCH_SIZE \
-       --eval_steps 250 --eval_strategy "steps" \
-       --logging_strategy "steps" --logging_steps 10 \
-       --label_smoothing_factor $LABEL_SMOOTHING --warmup_ratio $WARMUP_RATIO --weight_decay $WEIGHT_DECAY --dropout $DROPOUT \
-       --load_best_model_at_end True --metric_for_best_model "eval_f1" \
-       --optim "adamw_hf" --lr_scheduler_type $SCHEDULER \
-       --data_dir /volume/ner/$DATASET --output_dir /volume/ner/outputs/$ITER_NAME
-   fi
 
 
+# Run testing
+if [ -d /volume/ner/outputs/$ITER_NAME/checkpoint-avg ]; then
+  python src/ner_training/main.py --run_test \
+      --model_name_or_path FacebookAI/roberta-base \
+      $CRF \
+      --checkpoint /volume/ner/outputs/$ITER_NAME/checkpoint-avg \
+      $CLASSES \
+      --data_dir /volume/ner/$DATASET \
+      --output_dir /volume/ner/outputs/$ITER_NAME \
+      --output_name "average" $PREDICT_ON_TRAIN
+fi
 
-  # Run testing
-  if [ -d /volume/ner/outputs/$ITER_NAME/checkpoint-avg ]; then
-    python src/ner_training/main.py --run_test \
-        --model_name_or_path FacebookAI/roberta-base \
-        $CRF \
-        --checkpoint /volume/ner/outputs/$ITER_NAME/checkpoint-avg \
-        $CLASSES \
-        --data_dir /volume/ner/$DATASET \
-        --output_dir /volume/ner/outputs/$ITER_NAME \
-        --output_name "average" $PREDICT_ON_TRAIN
-  fi
-
-  if [ -d /volume/ner/outputs/$ITER_NAME/checkpoint-best ]; then
-    python src/ner_training/main.py --run_test \
-        --model_name_or_path FacebookAI/roberta-base \
-        $CRF \
-        --checkpoint /volume/ner/outputs/$ITER_NAME/checkpoint-best \
-        $CLASSES \
-        --data_dir /volume/ner/$DATASET \
-        --output_dir /volume/ner/outputs/$ITER_NAME \
-        --output_name "best" $PREDICT_ON_TRAIN
-  fi
-
-done;
+if [ -d /volume/ner/outputs/$ITER_NAME/checkpoint-best ]; then
+  python src/ner_training/main.py --run_test \
+      --model_name_or_path FacebookAI/roberta-base \
+      $CRF \
+      --checkpoint /volume/ner/outputs/$ITER_NAME/checkpoint-best \
+      $CLASSES \
+      --data_dir /volume/ner/$DATASET \
+      --output_dir /volume/ner/outputs/$ITER_NAME \
+      --output_name "best" $PREDICT_ON_TRAIN
+fi
